1a074915b47a52350f22b6d02c98e5bb342635aa
Alejandro <diazluisalejandro25@gmail.com>
2022-01-25 17:27:55 -0500

    Prueba de Curso

--- Codigo.js
+++ Codigo.js
@@ -1,1 +1,20 @@
-alert("Hello world");
+let nombre = "Alejandro";
+let apellido = "Diaz";
+let nombrePlazi = "Ceynk";
+let correo = "diazluisalejandro25@gmail.com";
+let edadMayor = 18;
+let dinero = 100000;
+let deudas = 85000;
+let total = dinero - deudas;
+
+function miPerfil(Perfil) {
+  return nombre + " " + apellido + " " + "Edad:" + `${edadMayor}`;
+}
+
+function miCorreo(Correo) {
+  return "Correo: " + correo + "  " + "Nombre de Usuario:" + nombrePlazi;
+}
+
+function miDineroTotal(Dinero){
+    return "Dinero:" + dinero + " - "+ "Deudas:" + deudas + " " +"DineroTotal:" + total;
+}
