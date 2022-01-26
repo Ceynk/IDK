8b7f22eb450925e755c780dba333d63739e017dc
Alejandro <diazluisalejandro25@gmail.com>
2022-01-26 16:08:54 -0500

    Funciones de Concatenacion

--- Codigo.js
+++ Codigo.js
@@ -1,5 +1,19 @@
-function nuevaFuncion(name, age, country){
+function nuevaFuncion(name, age, country) {
   var name = name || "Alejandro";
   var age = age || 16;
-  var country = country || "Colombia"
+  var country = country || "Colombia";
+  console.log(name, age, country);
 }
+//es6
+function nuevaFuncion2(name = "Alejandro", age = 16, country = "Colombia") {
+  console.log(name, age, country);
+}
+
+nuevaFuncion2();
+nuevaFuncion2("Luis", "15", "Panama"); 
+
+let hello = "Hola"; 
+let World = "Mundo"; 
+let FraseEpica = hello + " " + World + ":D"; 
+let FraseEpica2 = `${hello} ${World}`;
+console.log(FraseEpica2)
