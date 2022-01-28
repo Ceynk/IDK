830685c14b5135971723b5c7a81f640092c0c891
Alejandro <diazluisalejandro25@gmail.com>
2022-01-28 17:30:42 -0500

    Otra

--- Codigo.js
+++ Codigo.js
@@ -2,17 +2,10 @@ function miPerfil(nombre = "Alejandro", apellido = "Diaz", edadMayor = 18) {
   return `${nombre} ${apellido} Edad:${edadMayor}`;
 }
 
-function miCorreo(
-  correo = "diazluisalejandro25@gmail.com",
-  nombrePlazi = "Ceynk"
-) {
+function miCorreo(correo = "diazluisalejandro25@gmail.com", nombrePlazi = "Ceynk") {
   return `Correo:${correo} Alias: ${nombrePlazi}`;
 }
 
-function miDineroTotal(
-  dinero = 100000,
-  deudas = 85000,
-  total = dinero - deudas
-) {
+function miDineroTotal(dinero = 100000, deudas = 85000, total = dinero - deudas) {
   return `Dinero:${dinero} - Deudas:${deudas}  DineroTotal:${total}`;
-}
+} 
