3242b9662017d7c5627ee52a40384ba1dc878038
Alejandro <diazluisalejandro25@gmail.com>
2022-01-28 19:09:27 -0500

    God

--- Codigo.js
+++ Codigo.js
@@ -9,3 +9,37 @@ function miCorreo(correo = "diazluisalejandro25@gmail.com", nombrePlazi = "Ceynk
 function miDineroTotal(dinero = 100000, deudas = 85000, total = dinero - deudas) {
   return `Dinero:${dinero} - Deudas:${deudas}  DineroTotal:${total}`;
 } 
+
+const tipoDeSuscripcion = "Basic";
+  
+  if(tipoDeSuscripcion == "Gratis"){
+    console.log("Solo puedes tomar los cursos gratis")
+  } 
+  else if(tipoDeSuscripcion == "Basic"){
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un mes")
+  } 
+  else if(tipoDeSuscripcion == "Expert"){
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un año")
+  } 
+  else if(tipoDeSuscripcion == "Expert Plus"){
+    console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año")
+  } 
+  else{
+    console.log("Pobre no tiene plazi")
+  } 
+ 
+  //
+
+i = 14;
+while (i >= 2) {
+	console.log("El valor de i es: " + i);
+	i--;
+}
+// 
+a = prompt("Cuanto es 2 + 2 ")
+ if(a == 4){
+   alert("Estas en lo correcto");
+ }
+ else{
+   alert("eres realmente estupido")
+ }
