c59774e93259306b9f9255908857148e4ff1c840
Alejandro <diazluisalejandro25@gmail.com>
2022-02-05 16:52:09 -0500

    Goddd

---
+++ Codigo.js
@@ -0,0 +1,22 @@
+//Codgio del Cuadrado
+const ladoCuadrado = prompt("Lados");
+console.log(`Los Lados del cuadrado miden: ${ladoCuadrado}cm`);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log(`Los Lados del cuadrado miden: ${perimetroCuadrado}cm`);
+alert(perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log(`Los Area del cuadrado miden: ${areaCuadrado} cm^2`);
+alert(areaCuadrado);
+
+//Codgio del Triangulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+console.log(
+  `los lados del triangulo miden: 
+  ${ladoTriangulo1}cm 
+  ${ladoTriangulo2}cm
+  ${baseTriangulo}cm `
+  );
