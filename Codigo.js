026f721b59b410bc203366a342f3bcc179c286a2
Alejandro <diazluisalejandro25@gmail.com>
2022-06-21 18:04:13 -0500

    sisa

--- Codigo.js
+++ Codigo.js
@@ -1,3 +1,13 @@
+function calcularMedia(lista) {
+    const sumaLista = lista.reduce(
+        function (valorAcomulado = 0, nuevoElemento) {
+            return valorAcomulado + nuevoElemento
+        }
+    );
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
+
 const lista1 = [
     100,
     200,
@@ -17,8 +27,14 @@ function esPar(numerito) {
 let mediana;
 
 if (esPar(lista1.length)) {
-    const elemento1 = lista1[mitadLista1];
-    const elemento2 = lista1[mitadLista1 + 1];
+    const elemento1 = lista1[mitadLista1 - 1];
+    const elemento2 = lista1[mitadLista1];
+    
+    const promedio1y2 = calcularMedia([
+        elemento1,
+        elemento2,
+    ]);
+    mediana = promedio1y2;
 }
 else {
     mediana = lista1[mitadLista1]
