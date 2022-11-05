4cfc7d74ff3377dcf2d9367aae77c4514a37445a
Alejandro <diazluisalejandro25@gmail.com>
2022-11-04 19:42:15 -0500

    sioque

--- Codigo.js
+++ Codigo.js
@@ -1,3 +1,40 @@
-a = 3;  
-b = 4;
-c = a + b;
+const salariosCol = colombia.map(
+    function (personita) {
+        return personita.salary;
+    }
+);
+
+const salariosColSorted = salariosCol.sort(
+    function (salaryA, salaryB) {
+        return salaryA - salaryB;
+    }
+);
+
+function esPar(numerito) {
+    return (numerito % 2 === 0);
+};
+
+function calcularMedia(lista) {
+    const sumaLista = lista.reduce(
+        function (valorAcomulado = 0, nuevoElemento) {
+            return valorAcomulado + nuevoElemento
+        }
+    );
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
+
+function medianaSalarios(lista) {
+    const mitad = parseInt(lista.length / 2);
+
+    if (esPar(lista.length)) {
+        const personitaMitad1 = lista[mitad - 1];
+        const personitaMitad2 = lista[mitad];
+    
+        const mediana =  calcularMedia([personitaMitad1, personitaMitad2])
+        return mediana;
+    } else {
+        const personitaMitad = lista[mitad];
+        return personitaMitad;
+    }
+}
