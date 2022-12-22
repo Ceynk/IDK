01876506a8ff35d627fa92aa5a3b98ba0471c712
Alejandro <diazluisalejandro25@gmail.com>
2022-12-21 21:41:29 -0500

    hola

--- Codigo.js
+++ Codigo.js
@@ -1,4 +1,50 @@
-x = 5;
-y = 6;
-z = x + y;
-console.log(z);
+console.group("Cuadrado");
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+function areaCuadrado(lado) {
+    return lado * lado;
+}
+console.groupEnd();
+//Codigo del Triangulo
+console.group("Triangulo");
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+function areaTriangulo(base, altura) {
+    return (base * altura) / 2;
+}
+console.groupEnd();
+//Codigo del Circulo 
+console.group("Circulo");
+//Diametro
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+//PI
+const PI = Math.PI;
+//Circuferencia
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+//Area
+function areaCirculo(radio) {
+    return (radio * radio) * PI;
+}
+console.groupEnd();
+//Utilizamos HTML 
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+function calcularAreaCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = areaCuadrado(value);
+    alert(perimetro);
+} 
