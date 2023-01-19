f52c99cb638cd7bd227b30b2a8a183e066e8a1df
Alejandro <diazluisalejandro25@gmail.com>
2023-01-18 21:35:32 -0500

    mega cambio

--- Prueba/Codigo.js
+++ Prueba/Codigo.js
@@ -6,6 +6,7 @@ function areaCuadrado(lado) {
     return lado * lado;
 }
 console.groupEnd();
+
 //Codigo del Triangulo
 console.group("Triangulo");
 function perimetroTriangulo(lado1, lado2, base) {
@@ -15,24 +16,29 @@ function areaTriangulo(base, altura) {
     return (base * altura) / 2;
 }
 console.groupEnd();
+
 //Codigo del Circulo 
 console.group("Circulo");
 //Diametro
 function diametroCirculo(radio) {
     return radio * 2;
 }
+
 //PI
 const PI = Math.PI;
+
 //Circuferencia
 function perimetroCirculo(radio) {
     const diametro = diametroCirculo(radio);
     return diametro * PI;
 }
+
 //Area
 function areaCirculo(radio) {
     return (radio * radio) * PI;
 }
 console.groupEnd();
+
 //Utilizamos HTML 
 function calcularPerimetroCuadrado() {
     const input = document.getElementById("InputCuadrado");
@@ -47,4 +53,36 @@ function calcularAreaCuadrado() {
 
     const perimetro = areaCuadrado(value);
     alert(perimetro);
-} 
+}
+//Triangulo
+function calcularPerimetroTriangulo() {
+    const lado1 = document.getElementById("lado1");
+    const value = Number(lado1.value);
+
+    const lado2 = document.getElementById("lado2");
+    const value2 = Number(lado2.value);
+
+    const base = document.getElementById("base");
+    const value3 = Number(base.value);
+
+
+    const perimetro = perimetroTriangulo(value, value2, value3);
+    alert("El Perimetro del triangulo es " + perimetro);
+}
+
+function calcularAreaTriangulo() {
+    const lado1 = document.getElementById("lado1");
+    const value = Number(lado1.value);
+
+    const lado2 = document.getElementById("lado2");
+    const value2 = Number(lado2.value);
+
+    const base = document.getElementById("base");
+    const value3 = Number(base.value);
+
+    const altura = document.getElementById("altura");
+    const value4 = Number(altura.value);
+
+    const area = areaTriangulo(value3, value4);
+    alert("El area del triangulo es " + area);
+}
